77af6f2476dd385304dd152a00a3bd9d539418fa
xiaomzx <2844572730@qq.com>
2024-05-06 21:39:48 +0800

    注册服务功能

--- simple-provider/src/main/java/org/example/provider/EasyProviderExample.java
+++ simple-provider/src/main/java/org/example/provider/EasyProviderExample.java
@@ -1,11 +1,14 @@
 package org.example.provider;
 
+import org.example.common.services.UserService;
+import org.example.rpc.registry.LocalRegistry;
 import org.example.rpc.service.HttpServer;
 import org.example.rpc.service.VertxHttpServer;
 
 public class EasyProviderExample {
     public static void main(String[] args) {
-        //todo
+        //注册服务
+        LocalRegistry.register(UserService.class.getName(),UserServiceImpl.class);
         //具体提供服务代码
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
